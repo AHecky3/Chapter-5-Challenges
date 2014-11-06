5cb1a5603613561aa938936bebcfd176471043aa
AHecky3 <Ahecky15@stcharlessd.org>
2014-11-06 13:41:17 -0600

    deleted:    Challenge 1 	new file:   Challenge 1.py 	renamed:    Challenge 2 -> Challenge 2.py 	renamed:    Challenge 3 -> Challenge 3.py 	renamed:    Challenge 4 -> Challenge 4.py 	deleted:    README.md

--- Challenge 2.py
+++ Challenge 2.py
@@ -3,3 +3,12 @@
    on four attributes: Strength, Health, Wisdom, and Dexterity. The player should be able to spend points from the pool 
    on any attribute and should also be able to take points from an attribute and put them back into the pool.
 """
+skillPoints = 30
+skillsDB = {'Strength':[(0)],
+            'Health':[(0)],
+            'Wisdom':[(0)],
+            'Dexterity':[(0)]}
+ListOfKeys = skillsDB.keys()
+
+#for i in ListOfKeys :
+#    print(i)
