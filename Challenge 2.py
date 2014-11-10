e4572cc3fb615a8548425e797325a4aaf7ebed60
AHecky3 <ahecky@stcharlessd.org>
2014-11-10 13:32:15 -0600

    Update Challenge 2.py

--- Challenge 2.py
+++ Challenge 2.py
@@ -1,23 +1,51 @@
+#!/usr/local/bin/python3.3
 """
 2. Write a Character Creator program for a role-playing game. The player should be given a pool of 30 poitns to spend
-   on four attributes: Strength, Health, Wisdom, and Dexterity. The player should be able to spend points from the pool 
+   on four attributes: Strength, Health, Wisdom, and Dexterity. The player should be able to spend points from the pool
    on any attribute and should also be able to take points from an attribute and put them back into the pool.
 """
 #Setting Initial Values
 skillPoints = 30
-skillsDB = {'Strength':[(0)],
-            'Health':[(0)],
-            'Wisdom':[(0)],
-            'Dexterity':[(0)]}
+skillsDB = {'Strength':[('0')],
+            'Health':[('0')],
+            'Wisdom':[('0')],
+            'Dexterity':[('0')]}
 ListOfKeys = skillsDB.keys()
 
 errorMessage = ["\n    ** Please enter 'Yes' or 'No' ** \n", "\n    ** That is not a valid input. **\n"]
+
+def toDictionary() :
+    global skillPoints
+    print("How many points do you want to " + skillChange.lower(), tofrom, editSkill)
+    if whileAdd == True :
+        addPoints = int(input("Add:   "))
+        print("points")
+        removePoints = 0
+        skillPoints -= addPoints
+    if whileRemove == True:
+        addPoints = int(input("Remove:   "))
+        print("points")
+        addPoints = 0
+        skillPoints += removePoints
+    keyPull = skillsDB[editSkill] #Pulling List from Dictionary
+    skillPull = keyPull[0] #Pulling Key Value from List
+    editDict = int(skillPull) + addPoints - removePoints #Setting Value to change Dict
+    skillsDB[editSkill] = str(editDict) #Assigning Value to Dict
+
+def printSkills():
+    print("")
+    print("You have '" + str(skillPoints) + "' free skill points. ")
+    print("Current Stats: " + charName)
+    for i in skillsDB :
+        for j in skillsDB[i] :
+          print("    - " + i + " : " + j[0])
+    print('')
 
 def test():
     print("\
         ** This is a test ** \
         ")
-    
+
 #for i in ListOfKeys :
 #    print(i)
 
@@ -57,9 +85,29 @@ while nameChar == True :
                         print("\nPlease enter your characters name. ")
                         break
                 print(errorMessage[0])
-                
+
         else:
             print("\n    ** That is not a valid name ** \n")
 
 #### CREATE SKILL SELECTION / POINT USE LOOP ####
+while True:
+    printSkills()
+    print("What skill do you wish to edit? ")
+    editSkill = input("Skill: ").title()
+    print("")
+    print("Do you want to add or remove points from " + editSkill + "?")
+    skillChange = input("Add or Remove: ").title()
+    print("")
+    if skillChange == "Add" :
+        whileAdd = True
+        whileRemove = False
+        tofrom = "to"
+    elif skillChange == "Remove" :
+        whileAdd = False
+        whileRemove = True
+        tofrom = "from"
+    toDictionary()
+
+
+
 
